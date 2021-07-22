4d6b40ca8549749837bb95771b6d81076014e3a4
HunnidStax <den4ik.trofimenko@gmail.com>
2021-07-23 00:52:06 +0300

    Test1

--- L2/L2/Program.cs
+++ L2/L2/Program.cs
@@ -6,7 +6,30 @@ namespace L2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите  максимальную и минимальную температуру за день");
+
+            Console.Write("Минимальная:");
+            int Low = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Максимальная:");
+            int High = Convert.ToInt32(Console.ReadLine());
+
+            int Aver = (Low+High)/2;
+            Console.WriteLine($"Средняя температура за день: {Aver}");
+
+            Console.WriteLine("Какой сейчас месяц?");
+            string Month = Console.ReadLine();
+
+            if (Month == "Декабрь" || Month == "Январь" || Month == "Февраль" && Aver>0)
+            {
+                Console.WriteLine(Month);
+                Console.WriteLine("Дождливая зима.");
+            }
+            else
+            {
+                Console.WriteLine(Month);
+            }
+
         }
     }
 }
