bddf49d5a76cd531b54189f5207fca1f14a8131c
HunnidStax <den4ik.trofimenko@gmail.com>
2021-07-23 15:39:34 +0300

    Test3

--- L2/L2/Program.cs
+++ L2/L2/Program.cs
@@ -26,19 +26,72 @@ namespace L2
                     Console.WriteLine($"Средняя температура за день: {Aver}");
 
                     //2
-                    Console.WriteLine("Какой сейчас месяц?");
-                    string Month = Console.ReadLine();
+                    Console.WriteLine("Введите номер месяца");
+                    int Month = Convert.ToInt32(Console.ReadLine());
 
-                    if (Month == "Декабрь" || Month == "Январь" || Month == "Февраль" && Aver > 0)
+                    if (Month == 12 && Aver > 0)
                     {
-                        Console.WriteLine(Month);
+                        Console.WriteLine("Декабрь.");
                         Console.WriteLine("Дождливая зима.");
                     }
-                    else
+                    else if (Month == 12 && Aver <= 0)
                     {
-                        Console.WriteLine(Month);
+                        Console.WriteLine("Декабрь.");
                     }
-
+                    else if (Month == 1 && Aver > 0)
+                    {
+                        Console.WriteLine("Январь.");
+                        Console.WriteLine("Дождливая зима.");
+                    }
+                    else if (Month == 1 && Aver <= 0)
+                    {
+                        Console.WriteLine("Январь.");
+                    }
+                    else if (Month == 2 && Aver > 0)
+                    {
+                        Console.WriteLine("Февраль.");
+                        Console.WriteLine("Дождливая зима.");
+                    }
+                    else if (Month == 2 && Aver <= 0)
+                    {
+                        Console.WriteLine("Февраль.");
+                    }
+                    else if (Month == 3)
+                    {
+                        Console.WriteLine("Март.");
+                    }
+                    else if (Month == 4)
+                    {
+                        Console.WriteLine("Апрель.");
+                    }
+                    else if (Month == 5)
+                    {
+                        Console.WriteLine("Май.");
+                    }
+                    else if (Month == 6)
+                    {
+                        Console.WriteLine("Июнь.");
+                    }
+                    else if (Month == 7)
+                    {
+                        Console.WriteLine("Июль.");
+                    }
+                    else if (Month == 8)
+                    {
+                        Console.WriteLine("Август.");
+                    }
+                    else if (Month == 9)
+                    {
+                        Console.WriteLine("Сентябрь.");
+                    }
+                    else if (Month == 10)
+                    {
+                        Console.WriteLine("Октябрь.");
+                    }
+                    else if (Month == 11)
+                    {
+                        Console.WriteLine("Ноябрь.");
+                    }
                     break;
 
                 case 2:
@@ -66,10 +119,15 @@ namespace L2
                     double price1 = 51.90;
                     string product2 = "Хлеб";
                     double price2 = 33.50;
+                    DateTime date1 = new DateTime(2015, 7, 20);
 
-                    Console.WriteLine($"******************************\n                              \n.......Касоовый чек № {nomercheka}.......\nПродукт...................Цена\n                              \n{product1}..................{price1}\n{product2}....................{price2}\n                              \n******************************");
+                    Console.WriteLine($"******************************\n                              \n.......Касоовый чек № {nomercheka}.......\nПродукт...................Цена\n                              \n{product1}..................{price1}\n{product2}....................{price2}\n                              \nДата покупки: {date1.ToShortDateString()}......\n                              \n******************************");
 
                     break;
+                
+
+                    //6
+
             }
         }
     }
